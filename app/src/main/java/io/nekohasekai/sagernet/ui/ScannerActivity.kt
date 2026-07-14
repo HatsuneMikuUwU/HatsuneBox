85577b9d15cd2f8d0690f56495e7a45221f2b271
HatsuneMikuUwU <146099520+HatsuneMikuUwU@users.noreply.github.com>
2026-07-15 06:24:02 +0700

    update dependencies

--- app/src/main/java/io/nekohasekai/sagernet/ui/ScannerActivity.kt
+++ app/src/main/java/io/nekohasekai/sagernet/ui/ScannerActivity.kt
@@ -2,6 +2,7 @@ package io.nekohasekai.sagernet.ui
 
 import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.content.pm.ShortcutManager
 import android.graphics.ImageDecoder
 import android.os.Build
@@ -11,15 +12,13 @@ import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import androidx.core.content.getSystemService
 import androidx.core.net.toUri
-import com.google.zxing.Result
-import com.king.zxing.CameraScan
-import com.king.zxing.DefaultCameraScan
+import com.king.camera.scan.CameraScan
+import com.king.camera.scan.DefaultCameraScan
 import com.king.zxing.analyze.QRCodeAnalyzer
 import com.king.zxing.util.CodeUtils
-import com.king.zxing.util.LogUtils
-import com.king.zxing.util.PermissionUtils
 import io.nekohasekai.sagernet.R
 import io.nekohasekai.sagernet.database.DataStore
 import io.nekohasekai.sagernet.database.ProfileManager
@@ -29,12 +28,20 @@ import io.nekohasekai.sagernet.ktx.*
 import java.util.concurrent.atomic.AtomicBoolean
 import java.util.concurrent.atomic.AtomicInteger
 
-
-class ScannerActivity : ThemedActivity(),
-    CameraScan.OnScanResultCallback {
+class ScannerActivity : ThemedActivity() {
 
     lateinit var binding: LayoutScannerBinding
-    lateinit var cameraScan: CameraScan
+    lateinit var cameraScan: CameraScan<String>
+
+    private val requestCameraPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted ->
+        if (isGranted) {
+            cameraScan.startCamera()
+        } else {
+            finish()
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -48,7 +55,6 @@ class ScannerActivity : ThemedActivity(),
             setHomeAsUpIndicator(R.drawable.ic_navigation_close)
         }
 
-        // 二维码库
         initCameraScan()
         startCamera()
         binding.ivFlashlight.setOnClickListener { toggleTorchState() }
@@ -77,9 +83,9 @@ class ScannerActivity : ThemedActivity(),
                             contentResolver, uri
                         )
                     }
-                    val result = CodeUtils.parseCodeResult(bitmap)
+                    val resultText = CodeUtils.parseQRCode(bitmap)
                     onMainDispatcher {
-                        onScanResultCallback(result, true)
+                        onScanResultCallback(resultText, true)
                     }
                 }
                 finish()
@@ -104,22 +110,13 @@ class ScannerActivity : ThemedActivity(),
     var finished = AtomicBoolean(false)
     var importedN = AtomicInteger(0)
 
-    /**
-     * 接收扫码结果回调
-     * @param result 扫码结果
-     * @return 返回true表示拦截，将不自动执行后续逻辑，为false表示不拦截，默认不拦截
-     */
-    override fun onScanResultCallback(result: Result?): Boolean {
-        return onScanResultCallback(result, false)
-    }
-
-    fun onScanResultCallback(result: Result?, multi: Boolean): Boolean {
+    fun onScanResultCallback(text: String?, multi: Boolean): Boolean {
         if (!multi && finished.getAndSet(true)) return true
         if (!multi) finish()
         runOnDefaultDispatcher {
             try {
-                val text = result?.text ?: throw Exception("QR code not found")
-                val results = RawUpdater.parseRaw(text)
+                val actualText = text ?: throw Exception("QR code not found")
+                val results = RawUpdater.parseRaw(actualText)
                 if (!results.isNullOrEmpty()) {
                     val currentGroupId = DataStore.selectedGroupForImport()
                     if (DataStore.selectedGroup != currentGroupId) {
@@ -143,80 +140,45 @@ class ScannerActivity : ThemedActivity(),
             } catch (e: Throwable) {
                 Logs.w(e)
                 onMainDispatcher {
-                    var text = getString(R.string.action_import_err)
-                    text += "\n" + e.readableMessage
-                    Toast.makeText(app, text, Toast.LENGTH_SHORT).show()
+                    var errText = getString(R.string.action_import_err)
+                    errText += "\n" + e.readableMessage
+                    Toast.makeText(app, errText, Toast.LENGTH_SHORT).show()
                 }
             }
         }
         return true
     }
 
-    /**
-     * 初始化CameraScan
-     */
     fun initCameraScan() {
-        cameraScan = DefaultCameraScan(this, binding.previewView)
+        cameraScan = DefaultCameraScan(this, binding.previewView, binding.viewfinderView)
         cameraScan.setAnalyzer(QRCodeAnalyzer())
-        cameraScan.setOnScanResultCallback(this)
-        cameraScan.setNeedAutoZoom(true)
-    }
-
-    /**
-     * 启动相机预览
-     */
-    fun startCamera() {
-        if (PermissionUtils.checkPermission(this, Manifest.permission.CAMERA)) {
-            cameraScan.startCamera()
-        } else {
-            LogUtils.d("checkPermissionResult != PERMISSION_GRANTED")
-            PermissionUtils.requestPermission(
-                this, Manifest.permission.CAMERA, CAMERA_PERMISSION_REQUEST_CODE
-            )
+        cameraScan.setOnScanResultCallback { result ->
+            onScanResultCallback(result.result, false)
+            true
         }
     }
 
-    /**
-     * 释放相机
-     */
+    fun startCamera() {
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.CAMERA
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            cameraScan.startCamera()
+        } else {
+            Logs.d("Requesting camera permission")
+            requestCameraPermissionLauncher.launch(Manifest.permission.CAMERA)
+        }
+    }
+
     private fun releaseCamera() {
         cameraScan.release()
     }
 
-    /**
-     * 切换闪光灯状态（开启/关闭）
-     */
     protected fun toggleTorchState() {
         val isTorch = cameraScan.isTorchEnabled
         cameraScan.enableTorch(!isTorch)
         binding.ivFlashlight.isSelected = !isTorch
-    }
-
-    val CAMERA_PERMISSION_REQUEST_CODE = 0X86
-
-    override fun onRequestPermissionsResult(
-        requestCode: Int, permissions: Array<String>, grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (requestCode == CAMERA_PERMISSION_REQUEST_CODE) {
-            requestCameraPermissionResult(permissions, grantResults)
-        }
-    }
-
-    /**
-     * 请求Camera权限回调结果
-     * @param permissions
-     * @param grantResults
-     */
-    fun requestCameraPermissionResult(permissions: Array<String>, grantResults: IntArray) {
-        if (PermissionUtils.requestPermissionsResult(
-                Manifest.permission.CAMERA, permissions, grantResults
-            )
-        ) {
-            startCamera()
-        } else {
-            finish()
-        }
     }
 
     override fun onDestroy() {
